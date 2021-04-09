a81beb1df998a2f237a74e19e4ff706c4ce6ab9b
Brady Joslin <brady@bradyjoslin.com>
2021-04-08 19:08:59 -0500

    Update rsieve.rb

--- Formula/rsieve.rb
+++ Formula/rsieve.rb
@@ -5,7 +5,7 @@ class Rsieve < Formula
     desc "Copies all or portions of a remote GitHub repo. Useful for pulling down template projects or reusable bits of code."
     homepage "https://github.com/bradyjoslin/rsieve"
     url "https://github.com/bradyjoslin/rsieve/releases/download/v0.0.5/rsieve-v0.0.5-x86_64-apple-darwin.tar.gz"
-    sha256 "1407c23e27fbf0e2af2f30de1091888a71201d0864e8812bd204d0433d966d33"
+    sha256 "4d0c0d2efc0000107893c27bad093b52c597f97a867b3097b64795daebde166e"
     version "0.0.5"
   
     def install
